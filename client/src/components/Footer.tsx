5265f462e2b6e775732afde244a3941537841f65
askhan963 <awaisknas963@gmail.com>
2024-10-03 09:30:41 +0500

    Client change

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export interface IFooterProps {}
 
-export default function Footer(props: IFooterProps) {
+export default function Footer() {
   return (
     <div className="mx-auto bg-slate-50 py-4 border-t border-gray-300">
       <h4 className="text-black text-center text-sm font-medium">© 2024 URL Shortener. All rights reserved.</h4>
